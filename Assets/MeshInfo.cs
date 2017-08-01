755cf36e2a5cb1dcf973d0d848512178f3fb17c7
RomainKurtz <romain_kurtz@discovery.com>
2017-08-01 16:40:06 +0200

    fiiirst

---
+++ Assets/MeshInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshInfo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        test();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //test();
+
+    }
+
+    // Distorts the mesh vertically.
+    void test()
+    {
+        // Get instantiated mesh
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        // Randomly change vertices
+        Vector3[] vertices = mesh.vertices;
+        int p = 0;
+        while (p < vertices.Length)
+        {
+            vertices[p] += new Vector3(0, Random.Range(-0.3F, 0.3F), 0);
+            p++;
+        }
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
+    }
+}
